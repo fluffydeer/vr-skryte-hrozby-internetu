86f8ec04641b39efb950359fcf92f745c90f07a2
fluffydeer <xklementova@stuba.sk>
2021-03-14 17:40:43 +0100

    L2 - object Player positioned lower

---
+++ Assets/Custom/Scripts/L1/SnapVirusBack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapVirusBack : MonoBehaviour
+{
+    public GameObject virus;
+    //musi to byt napevno, lebo ak pouzivatel do toho drgne tak to odleti inde
+    private Vector3 initialPosition;
+    public double x;
+
+    public void SaveInitialPosition()
+    {
+        initialPosition = virus.transform.position;
+        Debug.Log("halooo " + initialPosition);
+    }
+
+    public void SnapVirusBackToInitialPosition()
+    {
+        virus.transform.position = initialPosition;
+        Debug.Log("halooo2 " + initialPosition);
+
+    }
+
+}
